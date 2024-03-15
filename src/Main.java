940dacae59d457a5f1ff86e76f48edd472a5a1f9
Waylon! <weese.619547@swl.k12.oh.us>
2024-03-15 00:34:51 -0400

    Initial

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double itemCost;
+        double shipCost;
+        double total;
+        System.out.print("Enter the price of your item: $");
+        Scanner scan = new Scanner(System.in);
+        if(scan.hasNextDouble()){
+            itemCost = scan.nextDouble();
+            if(itemCost < 0)
+                System.out.println("Error: Invalid Input");
+            else{
+                if (itemCost < 100){
+                    shipCost = (itemCost * 0.02);
+                }
+                else {
+                    shipCost = 0;
+                }
+                total = itemCost + shipCost;
+                System.out.println("Shipping Cost: $" + shipCost);
+                System.out.print("Total Price: $" + total);
+            }
+
+        } else System.out.println("Error: Invalid Input");
     }
 }
